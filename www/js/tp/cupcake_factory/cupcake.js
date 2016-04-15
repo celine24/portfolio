44bcdfbf520ba51e333af73e88c1e60aa934e227
celine24 <peyrot.celine@gmail.com>
2016-04-15 17:15:13 +0200

    changement dossier

---
+++ www/js/tp/cupcake_factory/cupcake.js
@@ -0,0 +1,94 @@
+/**
+ * Created by PeopleAre on 09/03/2016.
+ */
+var cupcake = document.getElementById('cupcake');
+var egg = document.getElementById('egg');
+var mold = document.getElementById('mold');
+var mixer = document.getElementById('mixer');
+
+var nbCupcake = document.getElementById('nbCupcake');
+var nbEgg = document.getElementById('nbEgg');
+var nbMold = document.getElementById('nbMold');
+var nbMixer = document.getElementById('nbMixer');
+
+var priceEgg = document.getElementById('priceEgg');
+var priceMold = document.getElementById('priceMold');
+var priceMixer = document.getElementById('priceMixer');
+
+var ad = document.getElementById('ad');
+var score = 0;
+var nbEggs = 1;
+var nbMolds = 1;
+var nbMixers = 1;
+var compteur = 1;
+var costEgg = 10;
+var costMold = 50;
+var costMixer = 100;
+
+function convertPlural(nb, word) {
+    if (nb > 1) {
+        return word + 's';
+    }
+    else {
+        return word;
+    }
+}
+
+function cook () {
+    score += compteur;
+    nbCupcake.innerHTML = score + ' ' + convertPlural(score, 'cupcake');
+}
+
+
+function buyEgg() {
+    if (score >= costEgg) {
+        compteur += 1;
+        score -= costEgg;
+        costEgg *= 2;
+        nbEggs += 1;
+        nbCupcake.innerHTML = score + ' ' + convertPlural(score, 'cupcake');
+        nbEgg.innerHTML = nbEggs + ' ' + convertPlural(nbEggs, 'egg');
+        priceEgg.innerHTML = 'cost ' + costEgg;
+        ad.innerHTML = 'Congrats ! You bought another egg for ' + costEgg / 2 + '.';
+    }
+    else {
+        ad.innerHTML = 'Not enough cupcakes for egg.';
+    }
+}
+
+function buyMold() {
+    if (score >= costMold) {
+        compteur += 5;
+        score -= costMold;
+        costMold *= 2;
+        nbMolds += 1;
+        nbCupcake.innerHTML = score + ' ' + convertPlural(score, 'cupcake');
+        ad.innerHTML = 'Congrats ! You bought another cake mold for ' + costMold / 2 + '.';
+        nbMold.innerHTML = nbMolds + ' ' + convertPlural(nbMolds, 'mold');
+        priceMold.innerHTML = 'cost ' + costMold;
+    }
+    else {
+        ad.innerHTML = 'Not enough cupcakes for mold.';
+    }
+}
+
+function buyMixer() {
+    if (score >= costMixer) {
+        setInterval(cook, 1000);
+        score -= costMixer;
+        costMixer *= 10;
+        nbMixers += 1;
+        nbCupcake.innerHTML = score + ' ' + convertPlural(score, 'cupcake');
+        nbMixer.innerHTML = nbMixers + convertPlural(nbMixers, ' electric mixer');
+        priceMixer.innerHTML = 'cost ' + costMixer;
+        ad.innerHTML = 'Congrats ! You bought another electric mixer for ' + costMixer / 10 + '.';
+    }
+    else {
+        ad.innerHTML = 'Not enough cupcakes for electric mixer.';
+    }
+}
+
+cupcake.onclick = cook;
+egg.onclick = buyEgg;
+mold.onclick = buyMold;
+mixer.onclick = buyMixer;
---
+++ www/js/tp/todolist/controller/toDo.js
@@ -0,0 +1,67 @@
+app.directive('ngBlur', function() {
+    return function(scope, elem, attrs) {
+        elem.bind("blur", function(){
+            scope.$apply(attrs.ngBlur);
+        })
+    }
+});
+
+app.controller('toDoCtrl', ['$scope', '$http', 'filterFilter', '$location', function ($scope, $http, filterFilter, $location) {
+    $scope.tasks = [];
+    $scope.todoName = "Chargement...";
+    $scope.statusFilter = {};
+    
+    $http.get('tasks.php').success(function(data){
+        $scope.tasks = data;
+        $scope.todoName = "My To Do List";
+    });
+    
+    $scope.addTask = function (newTask) {
+        if (newTask != null) {
+            $scope.tasks.push(
+                {
+                    name : newTask,
+                    checked : false
+                }
+            );
+            $scope.newTask = "";
+        }
+        else {
+            alert("Merci de saisir une tâche.");
+        }
+    };
+    
+    $scope.rmTask = function (task) {
+        $scope.tasks.splice(task, 1);
+    };
+    
+    $scope.$watch("tasks", function() {
+        $scope.tasksLength = filterFilter($scope.tasks, {checked:false}).length;
+        $scope.allchecked = !$scope.tasksLength;
+    }, true);
+
+
+    if ($location.path() == '') { $location.path("/"); }
+    $scope.location = $location;
+    $scope.$watch('location.path()', function(path) {
+        $scope.statusFilter =
+            (path == "/active") ? {checked : false} :
+            (path == "/done") ? {checked : true} :
+            (path == "/") ? { } :
+            null
+    });
+    
+    $scope.checkAll = function (allchecked) {
+        $scope.tasks.forEach(function(task) {
+            task.checked = allchecked;
+        });
+    };
+
+    $scope.editTask = function (task) {
+        task.editing = false;
+    }
+
+    $scope.removeAll = function () {
+        $scope.tasks = [];
+    }
+}]);
