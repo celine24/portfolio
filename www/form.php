f2e88dbea554f2f02c42aea1977eca283ef2b7c9
celine24 <peyrot.celine@gmail.com>
2016-04-26 08:57:16 +0200

    installation phpmailer + tests envoi infructueux

---
+++ www/form.php
@@ -0,0 +1,95 @@
+<?php
+
+session_start();
+$errors = array();
+
+if(isset($_POST["submit"])) {
+    $lastname = htmlspecialchars($_POST["lastname"]);
+    $firstname = htmlspecialchars($_POST["firstname"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    if (!isset($lastname) || empty($lastname)) {
+        $errors ['lastname'] = "Merci de renseigner votre nom.";
+    } else {
+        if (!preg_match("#^[a-zA-ZÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ-]+$#", $lastname)) {
+            $errors ['lastname'] = "Le nom est invalide.";
+        }
+    }
+
+    if (!isset($firstname) || empty($firstname)) {
+        $errors ['firstname'] = "Merci de renseigner votre prénom.";
+    } else {
+        if (!preg_match("#^[a-zA-ZÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ-]+$#", $firstname)) {
+            $errors ['firstname'] = "Le prénom est invalide.";
+        }
+    }
+
+    if (!isset($email) || empty($email)) {
+        $errors ['email'] = "Merci de renseigner votre email";
+    } else {
+        if (!preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email)) {
+            $errors ['email'] = "L'adresse email est invalide.";
+        }
+    }
+
+    if (!isset($message) || empty($message) || $message === "Votre Message") {
+        $errors ['message'] = "Merci de remplir votre demande.";
+    }
+
+    if (!empty($errors)) { // si erreur on renvoie vers la page précédente
+        $_SESSION['errors'] = $errors;//on stocke les erreurs
+        $_SESSION['inputs'] = $_POST;
+        header('Location: index.php#contact');
+    }
+    else {
+        require_once('vendor/phpmailer/phpmailer/class.phpmailer.php');
+        $mail = new PHPMailer();
+        $mail->SetFrom('$email', '$firstname . \' \' . $lastname');
+        $mail->AddAddress("peyrot.celine@gmail.com", "Céline Peyrot");
+        $mail->Subject = "PHPMailer Test Subject via mail(), basic";
+        $mail->AddReplyTo($email, 'Reply to name');
+        $mail->AltBody = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+        $mail->MsgHTML($message);
+        if(!$mail->Send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        } else {
+            echo "Message sent!";
+        }
+
+
+//        $headers = 'MIME-Version: 1.0' . "\r\n";
+//        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+//        $headers .= 'FROM:' . $email;
+//        $to = 'peyrot.celine@gmail.com';
+//        $subject = 'Message envoyé par ' . $firstname . ' ' . $lastname . ' - <i>' . $email . '</i>';
+//        $message_content = '
+//          <table>
+//          <tr>
+//          <td><b>Emetteur du message:</b></td>
+//          </tr>
+//          <tr>
+//          <td>' . $subject . '</td>
+//          </tr>
+//          <tr>
+//          <td><b>Contenu du message:</b></td>
+//          </tr>
+//          <tr>
+//          <td>' . htmlspecialchars($message) . '</td>
+//          </tr>
+//          </table>';
+//        $sending = mail($to, $subject, $message_content, $headers);
+//        if ($sending) {
+//            $sendingStatus = "L'email a bien été envoyé.";
+//        }
+//        else {
+//            $sendingStatus = "Une erreur s'est produite. L'email n'a pas pu être envoyé :(";
+//        }
+//        $_SESSION['sending'] = $sendingStatus;
+//        header('Location: index.php#contact');
+    }
+}
+else
+{
+    echo 'Vous devez remplir le formulaire.';
+}
--- www/index.php
+++ www/index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -333,18 +336,48 @@
                 <div class="content">
                     <h1>Me contacter</h1>
                     <div class="wrapper-content">
-                        <form class="contact-form" enctype="text/plain" method="post" action="mailto:peyrot.celine@gmail.com">
-                            <input type="text" placeholder="Votre Nom" name="lastname" />
-                            <input type="text" placeholder="Votre Prénom" name="firstname" />
-                            <input type="text" placeholder="Votre Email" name="email" />
-                            <textarea name="message">Votre Message</textarea>
-                            <input type="submit" value="Envoyer" />
+                        <form class="contact-form" action="form.php" method="POST">
+                            <div class=""><?php echo isset($_SESSION["sending"]) ? $_SESSION["sending"] : ''; ?></div>
+                            <div class="form-input">
+                                <input type="text" placeholder="Votre Nom" name="lastname" required value="<?php echo isset($_SESSION["inputs"]["lastname"]) ? $_SESSION["inputs"]["lastname"] : ''; ?>" />
+                                <?php if (isset($_SESSION["errors"]["lastname"])): ?>
+                                    <div class="error">
+                                        <?php echo $_SESSION["errors"]["lastname"]; ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <div class="form-input">
+                                <input type="text" placeholder="Votre Prénom" name="firstname" required value="<?php echo isset($_SESSION["inputs"]["firstname"]) ? $_SESSION["inputs"]["firstname"] : ''; ?>" />
+                                <?php if (isset($_SESSION["errors"]["firstname"])): ?>
+                                    <div class="error">
+                                        <?php echo $_SESSION["errors"]["firstname"]; ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <div class="form-input">
+                                <input type="text" placeholder="Votre Email" name="email" required value="<?php echo isset($_SESSION["inputs"]["email"]) ? $_SESSION["inputs"]["email"] : ''; ?>" />
+                                <?php if (isset($_SESSION["errors"]["email"])): ?>
+                                    <div class="error">
+                                        <?php echo $_SESSION["errors"]["email"]; ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <div class="form-textarea">
+                                <textarea name="message" required><?php echo isset($_SESSION["inputs"]["lastname"]) ? $_SESSION["inputs"]["lastname"] : 'Votre Message'; ?></textarea>
+                                <?php if (isset($_SESSION["errors"]["message"])): ?>
+                                    <div class="error">
+                                        <?php echo $_SESSION["errors"]["message"]; ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <input type="submit" name="submit" value="Envoyer" />
                         </form>
                         <div class="contact-info">
                             <div>Céline Peyrot</div>
                             <div>Intégrateur-Développeur Web</div>
                             <div>peyrot.celine@gmail.com</div>
                             <div>(Gironde, France)</div>
+
                         </div>
                     </div>
                     <!-- ... -->
@@ -364,3 +397,8 @@
     <script type="text/javascript" src="js/menu_mobile/menu.js"></script>
 </body>
 </html>
+<?php
+unset($_SESSION['inputs']); // on nettoie les données précédentes
+unset($_SESSION['sending']);
+unset($_SESSION['errors']);
+
